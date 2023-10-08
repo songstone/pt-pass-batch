78701ce8405e85a11849449ba7152b85a4c34d98
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-10-08 23:57:10 +0900

    feat: 엔티티 업데이트

--- src/main/java/com/song/pass/domain/booking/BookingRepository.java
+++ src/main/java/com/song/pass/domain/booking/BookingRepository.java
@@ -1,6 +1,19 @@
 package com.song.pass.domain.booking;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query( value = "UPDATE Booking b" +
+        "               SET b.usedPass = :usedPass," +
+        "                   b.modifiedAt = CURRENT_TIMESTAMP " +
+        "               WHERE b.passSeq = :passSeq"
+    )
+    int updateUsedPass(@Param("possSeq") Integer passSeq, @Param("usedPass") boolean usedPass);
 }
